3509a0a886c20be5f6e02619e1f2a96ed831f3da
itaywol <wo.itay@gmail.com>
2019-07-16 14:07:11 +0300

    implemented basic ui for registration

--- src/containers/identityFormContainer/identityFormContainer.tsx
+++ src/containers/identityFormContainer/identityFormContainer.tsx
@@ -2,29 +2,72 @@ import React from "react";
 import axios from "axios";
 import "./identityFormContainer.scss";
 import { Redirect } from "react-router";
+
 export default class IdentityRegistrationController extends React.Component {
   state = {
     phoneNumber: "",
-    redirect: false
+    code: 0,
+    redirect: 0
   };
 
   handlePhoneChange = (e: any) => {
     this.setState({ phoneNumber: e.target.value });
   };
-  
+
+  handleCodeChange = (e: any) => {
+    this.setState({ code: e.target.value });
+  };
+
+  handleCodeSubmit = (e: any) => {
+    if (e) e.preventDefault();
+    axios
+      .get(`/phone/${this.state.phoneNumber}/${this.state.code}`)
+      .then(response => {
+        let result = JSON.stringify(response.data);
+        console.log(result);
+        if (result === "[true]") {
+          localStorage.setItem("accessToken", result);
+          this.setState({ redirect: 2 });
+        } else {
+        }
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
   handlePhoneSubmit = (e: any) => {
     if (e) e.preventDefault();
     axios
       .post("/phone", { phoneNumber: this.state.phoneNumber })
       .then(response => {
-        let result = JSON.stringify(response.data);
-        result = result.slice(1, result.length - 1);
-        localStorage.setItem(result.split(":")[0], result.split(":")[1]);
-        this.setState({ redirect: true });
+        this.setState({ redirect: 1 });
       })
       .catch(err => {
         console.log(err);
       });
+  };
+
+  fillVerifyCodeForm = () => {
+    return (
+      <React.Fragment>
+        <div id="phoneForm">
+          <h4>We jest sent you an sms with Verfication code</h4>
+          <p>Please enter your code number below:</p>
+          <input
+            type="text"
+            value={this.state.code != null ? this.state.code : ""}
+            onChange={this.handleCodeChange}
+          />
+          <input
+            id="confirm"
+            type="submit"
+            value="Confirm"
+            onClick={this.handleCodeSubmit}
+          />
+        </div>
+      </React.Fragment>
+    );
   };
 
   fillPhoneForm = () => {
@@ -32,10 +75,9 @@ export default class IdentityRegistrationController extends React.Component {
       <React.Fragment>
         <div id="phoneForm">
           <h4>Welcome</h4>
-          <p>Please enter your phone number below:</p>
+          <p>Please enter your phone number below(using 972 country code):</p>
           <input
-            type="text"
-            value={this.state.phoneNumber != null ? this.state.phoneNumber : ""}
+            value={this.state.phoneNumber}
             onChange={this.handlePhoneChange}
           />
           <input
@@ -50,9 +92,15 @@ export default class IdentityRegistrationController extends React.Component {
   };
 
   render() {
-    if (this.state.redirect === false) {
+    
+    if (this.state.redirect === 0) {
       return this.fillPhoneForm();
-    } else {
+    } else if (this.state.redirect === 1) {
+      return this.fillVerifyCodeForm();
+    } else if (
+      this.state.redirect === 2 ||
+      localStorage.getItem("accessToken") === "[true]"
+    ) {
       return <Redirect to="/" />;
     }
   }
--- src/views/index/index.tsx
+++ src/views/index/index.tsx
@@ -3,20 +3,22 @@ import { Redirect } from "react-router-dom";
 
 class IndexPage extends React.Component {
   appView = () => {
-    return <React.Fragment />;
+    return <React.Fragment>hey</React.Fragment>;
   };
-
 
   /* 
     checks if there is user token registered on the browser localstorage if do move to the appview if not go to identity form(phone verfication)
   
   */
   checkAuth = () => {
-    if (localStorage.getItem("AccessToken")==="true") {
+    if (localStorage.getItem("accessToken") === "true") {
       console.log("not logged in redirecting");
       return <Redirect to="/identity" />;
+    } else if (localStorage.getItem("accessToken") === null) {
+      return <Redirect to="/identity" />;
+    } else {
+      return this.appView();
     }
-    return this.appView();
   };
 
   render() {
